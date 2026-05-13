3d14238907dfe05666e538dfdc79063730c82dcc
John Fulton <john@johnfulton.org>
2026-05-13 07:05:34 -0400

    update

--- acpp.sh
+++ acpp.sh
@@ -1,4 +1,10 @@
+read -p "Enter commit message (default: update): " commit_message
+
+if [ -z "$commit_message" ]; then
+    commit_message="update"
+fi
+
 git add -A
-git commit -m "update"
+git commit -m "$commit_message"
 git pull origin main
 git push origin main
